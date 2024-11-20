3285c62efa5bbe3047e0f4f375cb8969e5a668db
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-11-19 19:49:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,18 @@
 import streamlit as st
   
 giving = st.Page(
-  'pages/1_Giving.py', title = 'Giving Report', icon=":material/money:", default=True
+  'pages/1_Giving.py', title = 'Giving Report',  default=False
+)
+
+headcount = st.Page(
+  'pages/1_Headcount.py', title = 'Headcount Report', default=False
 )
 
 
 
 pg = st.navigation(
         {
-            " ": [giving],
+            " ": [giving, headcount],
 
         }
     )

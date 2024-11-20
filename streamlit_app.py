f99ba14e387423c2dcd3c9701c30f0be1e7b7805
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-11-19 20:42:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,10 @@ headcount = st.Page(
   'pages/2_Headcount.py', title = 'Headcount Report', default=False
 )
 
+groups = st.Page(
+  'pages/3_Groups.py', title = 'Groups Report', default=False
+)
+
 
 people = st.Page(
   'pages/4_People.py', title = 'People Report', default=False
@@ -17,7 +21,7 @@ people = st.Page(
 
 pg = st.navigation(
         {
-            " ": [giving, headcount, people],
+            " ": [giving, headcount, groups, people],
 
         }
     )

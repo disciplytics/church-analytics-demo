7f2c48695e7dc43a74644c824cf7555994886b67
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-11-19 22:01:57 -0500

    Create inactive_report.py

---
+++ people/inactive_report.py
@@ -0,0 +1,83 @@
+
+
+
+with st.expander("Click to Learn More"):
+    st.write(inactive_explaination_str)    
+
+inactive_data = people_data[(people_data['Status'] == 'inactive')]
+
+
+col6, col7, col8, col9 = st.columns(4)
+
+with col6:
+    sel6 = st.multiselect(
+            "Primary Campus",
+            inactive_data['Primary Campus'].unique(),
+            default = inactive_data['Primary Campus'].unique())
+with col7:
+    sel7= st.multiselect(
+            "Age Group",
+            inactive_data['Age Group'].unique(),
+            default = inactive_data['Age Group'].unique())
+with col8:
+    sel8 = st.multiselect(
+            "Membership",
+            inactive_data['Membership'].unique(),
+            default = inactive_data['Membership'].unique())
+with col9:
+    sel9 = st.multiselect(
+            "Marital Status",
+            inactive_data['Marital Status'].unique(),
+            default = inactive_data['Marital Status'].unique())
+
+
+    
+
+inactive_selection = inactive_data.query('`Primary Campus`== @sel6').query('`Age Group`== @sel7').query('`Membership`== @sel8').query('`Marital Status`== @sel9')
+
+st.subheader('Inactived People Trends')
+
+st.altair_chart(
+        alt.Chart(inactive_selection).transform_window(
+        Total='count(Status)',
+        sort=[{"field": "Inactivated At"}],
+        #groupby=['Year'],
+    ).mark_line().encode(
+        x='Inactivated At:T',
+        y='Total:Q',
+       # color='Year:N',
+    ),
+        use_container_width=True
+    )
+
+st.subheader('Inactive Reasons')
+st.altair_chart(
+            alt.Chart(inactive_selection, title = 'Total Inactive People By Inactive Reason').mark_bar().encode(
+                    y=alt.Y('Inactive Reason').title(""), 
+                    x=alt.X('count(PERSON_ID)').title('Total People')), 
+                use_container_width=True)
+
+st.subheader('Inactive People Breakdowns')
+col1i, col2i = st.columns(2)
+col1i.altair_chart(
+            alt.Chart(inactive_selection, title = 'Total Inactive People By Membership').mark_bar().encode(
+                    y=alt.Y('Membership').title(""), 
+                    x=alt.X('count(PERSON_ID)').title('Total People')), 
+                use_container_width=True)
+col2i.altair_chart(
+            alt.Chart(inactive_selection, title = 'Total Inactive People By Marital Status').mark_bar().encode(
+                    y=alt.Y('Marital Status').title(""), 
+                    x=alt.X('count(PERSON_ID)').title('Total People')), 
+                use_container_width=True)
+    
+col3i, col4i = st.columns(2)
+col3i.altair_chart(
+            alt.Chart(inactive_selection, title = 'Total Inactive People By Age Group').mark_bar().encode(
+                    y=alt.Y('Age Group').title(""), 
+                    x=alt.X('count(PERSON_ID)').title('Total People')), 
+                use_container_width=True)
+col4i.altair_chart(
+            alt.Chart(inactive_selection, title = 'Total Inactive People By Primary Campus').mark_bar().encode(
+                    y=alt.Y('Primary Campus').title(""), 
+                    x=alt.X('count(PERSON_ID)').title('Total People')), 
+                use_container_width=True)
